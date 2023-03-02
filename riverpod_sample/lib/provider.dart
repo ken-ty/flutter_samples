0557515c887ed5d8cadecbaf22930fec48ef9019
todoggler <kentokuraCS@gmail.com>
2023-03-03 03:33:48 +0900

    feat: タイトルだけstate管理

--- riverpod_sample/lib/provider.dart
+++ riverpod_sample/lib/provider.dart
@@ -1,22 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_sample/data/count_data.dart';
 
-final titleProvider = Provider<String>(
-  (ref) => 'Flutter デモページ',
-);
-
-final messageProvider = Provider<String>(
-  (ref) => 'You have pushed the button this many times:',
-);
-
-final countProvider = StateProvider<int>(
-  (ref) => 0,
-);
-
-final countDataProvider = StateProvider<CountData>(
-  (ref) => const CountData(
-    count: 0,
-    countUp: 0,
-    countDown: 0,
-  ),
-);
+final titleProvider = Provider<String>((ref) {
+  return 'タイトル';
+});
--- riverpod_sample/lib/view/home_page.dart
+++ riverpod_sample/lib/view/home_page.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_sample/data/count_data.dart';
 import 'package:riverpod_sample/provider.dart';
 
-class HomePage extends ConsumerStatefulWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  ConsumerState<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends ConsumerState<HomePage> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(title: Text(ref.watch(titleProvider))),
       body: Center(
