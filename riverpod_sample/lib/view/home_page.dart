bacda5e931af739117adf6cd6c296fea804ec212
todoggler <kentokuraCS@gmail.com>
2023-03-03 04:38:47 +0900

    feat: ツールチップ追加

--- riverpod_sample/lib/view/home_page.dart
+++ riverpod_sample/lib/view/home_page.dart
@@ -31,6 +31,7 @@ class HomePage extends StatelessWidget {
       ),
       floatingActionButton: Consumer(
         builder: (context, ref, child) => FloatingActionButton(
+          tooltip: '+1',
           onPressed: () => ref.read(countProvider.notifier).update((state) => state + 1),
           child: const Icon(Icons.add),
         ),
