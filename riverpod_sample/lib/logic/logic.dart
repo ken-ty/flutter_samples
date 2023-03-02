a9dfc7ee38b209743d4e75ee4ce582609f58265e
todoggler <kentokuraCS@gmail.com>
2023-03-03 07:39:38 +0900

    feat: logicにgetterとresetメソッド追加
    test実行して見つけた実装もれ

--- riverpod_sample/lib/logic/logic.dart
+++ riverpod_sample/lib/logic/logic.dart
@@ -7,6 +7,8 @@ class Logic {
     countDown: 0,
   );
 
+  get countData => _countData;
+
   increase() {
     _countData = _countData.copyWith(
       count: _countData.count + 1,
@@ -18,6 +20,14 @@ class Logic {
     _countData = _countData.copyWith(
       count: _countData.count - 1,
       countDown: _countData.countDown + 1,
+    );
+  }
+
+  reset() {
+    _countData = CountData(
+      count: 0,
+      countUp: 0,
+      countDown: 0,
     );
   }
 }
