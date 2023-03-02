61d40708e6806866471b19b29ff08721a7facf83
todoggler <kentokuraCS@gmail.com>
2023-03-03 02:56:22 +0900

    削除

--- riverpod_sample/lib/view/home_page.dart
+++ riverpod_sample/lib/view/home_page.dart
@@ -18,58 +18,8 @@ class _HomePageState extends ConsumerState<HomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(ref.watch(messageProvider)),
-            Text(
-              ref.watch(countDataProvider).count.toString(),
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    CountData countData = ref.read(countDataProvider);
-                    ref.read(countDataProvider.notifier).state =
-                        countData.copyWith(
-                      count: countData.count + 1,
-                      countUp: countData.countUp + 1,
-                    );
-                  },
-                  child: const Icon(Icons.plus_one),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    CountData countData = ref.read(countDataProvider);
-                    ref.read(countDataProvider.notifier).state =
-                        countData.copyWith(
-                      count: countData.count - 1,
-                      countDown: countData.countDown + 1,
-                    );
-                  },
-                  child: const Icon(Icons.exposure_minus_1_outlined),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text(ref.watch(countDataProvider).countUp.toString()),
-                Text(ref.watch(countDataProvider).countDown.toString()),
-              ],
-            ),
-          ],
+          children: const <Widget>[Text("a")],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          ref.watch(countDataProvider.notifier).state = const CountData(
-            count: 0,
-            countDown: 0,
-            countUp: 0,
-          );
-        },
-        child: const Icon(Icons.refresh),
       ),
     );
   }
