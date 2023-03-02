8abf1041e30b4e576c7d152ce118e8c40cb283d4
todoggler <kentokuraCS@gmail.com>
2023-03-03 04:54:34 +0900

    refactor: HomePageをConsumerStatefulWidgetで置き換える
    コードの見やすさの為

--- riverpod_sample/lib/view/home_page.dart
+++ riverpod_sample/lib/view/home_page.dart
@@ -2,24 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_sample/provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
 
   @override
+  ConsumerState<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends ConsumerState<HomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Consumer(
-          builder: (context, ref, child) => Text(ref.read(titleProvider)),
-        ),
-      ),
+      appBar: AppBar(title: Text(ref.read(titleProvider))),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Consumer(
-              builder: ((context, ref, child) => Text(ref.read(messageProvider))),
-            ),
+            Text(ref.read(messageProvider)),
             Consumer(
               builder: (context, ref, child) => Text(
                 ref.watch(countProvider).toString(),
